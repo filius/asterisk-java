29bafd52842e67d685991c750220038357c001c8
Valeriy Filatov <fil.src@gmail.com>
2015-05-05 15:38:54 +0300

    fields for AgentCompleteEvent

--- src/main/java/org/asteriskjava/manager/event/AgentCompleteEvent.java
+++ src/main/java/org/asteriskjava/manager/event/AgentCompleteEvent.java
@@ -37,6 +37,15 @@ public class AgentCompleteEvent extends AbstractAgentEvent
     private Long talkTime;
     private String reason;
     private String interfaceName;
+    private String calleridname;
+    private String exten;
+    private String calleridnum;
+    private String context;
+    private String connectedlinenum;
+    private String channelstatedesc;
+    private String connectedlinename;
+    private String priority;
+    private String channelstate;
 
     public AgentCompleteEvent(Object source)
     {
@@ -112,5 +121,77 @@ public class AgentCompleteEvent extends AbstractAgentEvent
 
     public void setInterfaceName(String interfaceName) {
         this.interfaceName = interfaceName;
+    }
+
+    public String getCalleridname() {
+        return calleridname;
+    }
+
+    public void setCalleridname(String calleridname) {
+        this.calleridname = calleridname;
+    }
+
+    public String getExten() {
+        return exten;
+    }
+
+    public void setExten(String exten) {
+        this.exten = exten;
+    }
+
+    public String getCalleridnum() {
+        return calleridnum;
+    }
+
+    public void setCalleridnum(String calleridnum) {
+        this.calleridnum = calleridnum;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getConnectedlinenum() {
+        return connectedlinenum;
+    }
+
+    public void setConnectedlinenum(String connectedlinenum) {
+        this.connectedlinenum = connectedlinenum;
+    }
+
+    public String getChannelstatedesc() {
+        return channelstatedesc;
+    }
+
+    public void setChannelstatedesc(String channelstatedesc) {
+        this.channelstatedesc = channelstatedesc;
+    }
+
+    public String getConnectedlinename() {
+        return connectedlinename;
+    }
+
+    public void setConnectedlinename(String connectedlinename) {
+        this.connectedlinename = connectedlinename;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getChannelstate() {
+        return channelstate;
+    }
+
+    public void setChannelstate(String channelstate) {
+        this.channelstate = channelstate;
     }
 }
